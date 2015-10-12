2118361b3d1a6d7094ed78fe505819dad533e7fb
sukohi <eagle.fly.free0722@gmail.com>
2015-10-12 16:59:41 -0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
 # jquery.em-en
-全角／半角を変換する jQuery プラグイン。 A jQuery plugin to convert EM and EN text.
+A jQuery plugin to convert EM and EN text.  
+[日本語](#japanese)
+
+Requirements
+====
+
+* [jQuery](https://jquery.com/)
+
+Installation
+=====
+Using bower is the simplest way.
+
+    bower install jquery.em-en --save-dev
+
+Preparation
+====
+
+Load jQuery and this package.
+
+    <script src="YOUR_PATH/jquery.min.js"></script>
+    <script src="YOUR_PATH/jquery.em-en.js"></script>
+
+Usage<a name="usage"></a>
+====
+
+**Text**
+
+    var enText = $.toEn('５５５ＭＭＭ');
+    var emText = $.toEm('555MMM');
+    
+**Selector**
+
+    var enText = $('#em_text').enVal();
+    var emText = $('#en_text').emVal();
+    
+**Convert automatically**
+
+    var enText = $('#em_text').enVal();
+    var emText = $('#en_text').emVal();
+    
+**with Callback**
+
+    $('#em_text').autoEn(function(e, enText){
+
+        alert(enText);
+
+    });
+    $('#en_text').autoEm(function(e, emText){
+
+        alert(emText);
+
+    });
+
+License
+====
+
+This package is licensed under the MIT License.
+
+Copyright 2015 Sukohi Kuhoh
+
+
+<a name="japanese"></a>
+
+# jquery.em-en
+全角／半角を変換する jQuery プラグイン。
+
+要件
+====
+
+* [jQuery](https://jquery.com/)
+
+インストール
+=====
+bowerコマンドを使えば簡単にインストールできます。
+
+    bower install jquery.em-en --save-dev
+    
+準備
+====
+
+jQuery と このパッケージを読み込む
+
+    <script src="YOUR_PATH/jquery.min.js"></script>
+    <script src="YOUR_PATH/jquery.em-en.js"></script>
+    
+使い方
+====
+
+[こちら](#usage)をご覧ください。
+
+ライセンス
+====
+
+MITライセンスにて配布されています。
+
+Copyright 2015 Sukohi Kuhoh
---
+++ jquery.em-en.js
@@ -0,0 +1,141 @@
+;(function($) {
+
+    var MNConverter = {
+
+        toM: function(text){
+
+            emText = text.replace(/[A-Za-z0-9]/g, function(s){
+
+                return String.fromCharCode(s.charCodeAt(0) + 0xFEE0);
+
+            });
+
+            if(MNConverter.checkReplacementCount()) {
+
+                $(MNConverter.replacements.en).each(function(index, target){
+
+                    var regex = new RegExp('['+ target +']', 'g');
+                    console.log(target);
+                    var replacement = MNConverter.replacements.em[index];
+                    emText = emText.replace(regex, replacement);
+
+                });
+
+            }
+
+            return emText;
+
+        },
+        toN: function(text){
+
+            var enText = text.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s){
+
+                return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+
+            });
+
+            if(MNConverter.checkReplacementCount()) {
+
+                $(MNConverter.replacements.em).each(function(index, target){
+
+                    var regex = new RegExp('['+ target +']', 'g');
+                    var replacement = MNConverter.replacements.en[index];
+                    enText = enText.replace(regex, replacement);
+
+                });
+
+            } else {
+
+                MNConverter.notMatchLog();
+
+            }
+
+            return enText;
+
+
+        },
+        checkReplacementCount: function(){
+
+            if(typeof(MNConverter.replacements.em) != 'object'
+                    || typeof(MNConverter.replacements.en) != 'object') {
+
+                return false;
+
+            }
+
+            return (MNConverter.replacements.em.length == MNConverter.replacements.en.length);
+
+        },
+        replacements: {}
+
+    };
+
+    jQuery.toEm = function(text) {
+
+        return MNConverter.toM(text);
+
+    }
+
+    jQuery.toEn = function(text) {
+
+        return MNConverter.toN(text);
+
+    }
+
+    jQuery.setEmEnReplacements = function(replacements) {
+
+        MNConverter.replacements = replacements;
+
+    }
+
+    $.fn.emVal = function() {
+
+        var text = $(this).val();
+        return MNConverter.toM(text);
+
+    }
+
+    $.fn.enVal = function() {
+
+        var text = $(this).val();
+        return MNConverter.toN(text);
+
+    }
+
+    $.fn.autoEm = function(callback) {
+
+        $(this).on('keyup', function(e){
+
+            var text = $(this).val();
+            var emText = MNConverter.toM(text);
+            $(this).val(emText);
+
+            if(typeof(callback) == 'function') {
+
+                callback(e, emText);
+
+            }
+
+        });
+
+    }
+
+    $.fn.autoEn = function(callback) {
+
+        $(this).on('keyup', function(e){
+
+            var text = $(this).val();
+            var enText = MNConverter.toN(text);
+            $(this).val(enText);
+
+            if(typeof(callback) == 'function') {
+
+                callback(e, enText);
+
+            }
+
+        });
+
+    }
+
+})(jQuery);
